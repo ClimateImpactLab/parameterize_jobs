10a4dea785f30c1746ca2ee1ff60394e3aa5beee
Brewster Malevich <sbmalev@gmail.com>
2023-07-21 11:14:56 -0700

    Refactor to fix rough check

--- src/parameterize_jobs/__init__.py
+++ src/parameterize_jobs/__init__.py
@@ -1,7 +1,7 @@
 
 """Top-level package for Parameterize Jobs."""
 
-from parameterize_jobs._version import version as __version__
+from parameterize_jobs._version import version
 from parameterize_jobs.parameterize_jobs import (
     Component,
     ComponentSet,
@@ -12,6 +12,7 @@ from parameterize_jobs.parameterize_jobs import (
 
 __author__ = """Michael Delgado"""
 __email__ = 'delgado.michaelt@gmail.com'
+__version__ = version
 
 _module_imports = (
     Component,
